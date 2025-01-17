db0c314cf8ec32c0d8e9baa621415360e59386b0
Alston <alston.peter@outlook.com>
2025-01-17 20:15:10 +0400

    Base model of main Market page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
+import Market from "./pages/Market/Market";
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/market" element={<Home />} />
+        <Route path="/market" element={<Market />} />
         <Route path="/profile" element={<Home />} />
         <Route path="/instructions" element={<Home />} />
         <Route path="/rules" element={<Home />} />
