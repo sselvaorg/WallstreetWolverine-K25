a99734e3e5cbc28efd5b8e529581d2bbf3ff9719
sselvaorg <sselvaganesh042@gmail.com>
2025-01-17 22:33:22 +0530

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
+import Market from "./pages/market/market";
+import Profile from "./pages/Profile/Profile";
+import Instructions from "./pages/Instructions/Instructions";
+import Rules from "./pages/Rules/Rules";
+import Contact from "./pages/Contact/Contact";
+import Login from "./pages/Login/Login";
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/market" element={<Home />} />
-        <Route path="/profile" element={<Home />} />
-        <Route path="/instructions" element={<Home />} />
-        <Route path="/rules" element={<Home />} />
-        <Route path="/contact" element={<Home />} />
-        <Route path="/login" element={<Home />} />
+        <Route path="/market" element={<Market />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/instructions" element={<Instructions />} />
+        <Route path="/rules" element={<Rules />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );
